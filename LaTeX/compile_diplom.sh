276f537fe3fec01c4b7bcf78b104c016cf1fc648
all3fox <all3fox@gmail.com>
2015-02-25 13:18:47 +0300

    fixed script to compile diplom

--- LaTeX/compile_diplom.sh
+++ LaTeX/compile_diplom.sh
@@ -2,6 +2,6 @@ rm diplom.aux diplom.aux.blg diplom.bbl diplom.bcf diplom.blg
 rm diplom.log diplom.out diplom.run.xml diplom.toc diplom.dvi
 pdflatex -shell-escape diplom.tex
 pdflatex -shell-escape diplom.tex
-biber diplom.aux
+biber diplom
 pdflatex -shell-escape diplom.tex
 pdflatex -shell-escape diplom.tex
